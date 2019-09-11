3e19311043fceb2e3a623895147693e853220502
areyde <49271013+areyde@users.noreply.github.com>
2019-09-11 17:28:03 +0300

    Designated a variable for a path to projects folder

--- DataCollection/download_projects.py
+++ DataCollection/download_projects.py
@@ -12,16 +12,18 @@ def cmdline(command):
 num_lines = sum(1 for line in open('java_projects_data.txt'))
 print('// Commencing download of', num_lines, 'projects.')
 
+path = '/home/ubuntu/projects/' 
+
 count = 0
 with open('java_projects_data.txt','r') as fin:
     for line in fin:
         count = count + 1
         data = line.split(';')
-        os.system('git clone https://user:password@github.com/' + data[0] + '/' + data[1] + ' /home/ubuntu/projects/' + data[0] + '/' + data[1])
+        os.system('git clone https://user:password@github.com/' + data[0] + '/' + data[1] + ' ' + path + data[0] + '/' + data[1])
         print('// Created folder for', data[0], '/', data[1])
-        os.system('curl -L -o /home/ubuntu/projects/' + data[0] + '/' + data[1] + '.zip https://areyde:Orwell19841984@github.com/' + data[0] + '/' + data[1] + '/zipball/master/')
+        os.system('curl -L -o ' + path + data[0] + '/' + data[1] + '.zip https://user:password@github.com/' + data[0] + '/' + data[1] + '/zipball/master/')
         print('// Created zip for', data[0], '/', data[1])
-        with open('/home/ubuntu/projects/' + data[0] + '/' + data[1] + '.txt', 'w') as fout:
+        with open(path + data[0] + '/' + data[1] + '.txt', 'w') as fout:
             fout.write(data[2])
         print('// Created text file for', data[0], '/', data[1])
         print('// Completed for project', count, 'out of', num_lines)
@@ -31,23 +33,23 @@ print('Completed initial download')
 with open('java_projects_data.txt','r') as fin:
     with open('list_checks.txt','w') as fout:
         for line in fin:
-            x = line.split(';')
-            fout.write(x[0] + '/' + x[1] + ';' + str(os.path.isdir('/home/ubuntu/projects/' + x[0] + '/' + x[1])) + ';' + str(os.path.isfile('/home/ubuntu/projects/' + x[0] + '/' + x[1] + '.zip')) + ';' + str(os.path.isfile('/home/ubuntu/projects/' + x[0] + '/' + x[1] + '.txt')) + '\n')
+            data = line.split(';')
+            fout.write(data[0] + '/' + data[1] + ';' + str(os.path.isdir(path + data[0] + '/' + data[1])) + ';' + str(os.path.isfile(path + data[0] + '/' + data[1] + '.zip')) + ';' + str(os.path.isfile(path + data[0] + '/' + data[1] + '.txt')) + '\n')
 
 with open('list_checks.txt','r') as fin:
     for line in fin:
         data = line.split(';')
-        if data[2] == 'False':
-            os.system('rm /home/ubuntu/projects/' + data[0] + '/' + data[1] + '.zip')
-            os.system('rm /home/ubuntu/projects/' + data[0] + '/' + data[1] + '.txt')
+        if data[1] == 'False':
+            os.system('rm ' + path + data[0] + '/' + data[1] + '.zip')
+            os.system('rm ' + path + data[0] + '/' + data[1] + '.txt')
 print('Deleted uncloned projects (deleted or made private)')
 
 with open('java_projects_data','r') as fin:
     with open('list_branches.txt','w') as fout:
         for line in fin:
             data = line.split(';')
-            if os.path.isdir(data[0] + '/' + data[1]) == True:
-                sout = cmdline('(cd /home/ubuntu/projects/' + data[0] + '/' + data[1] + '; git rev-parse --abbrev-ref HEAD)').decode('utf8').rstrip()
+            if os.path.isdir(path + data[0] + '/' + data[1]) == True:
+                sout = cmdline('(cd ' + path + data[0] + '/' + data[1] + '; git rev-parse --abbrev-ref HEAD)').decode('utf8').rstrip()
                 fout.write(data[0] + ';' + data[1] + ';' + sout + '\n')
 print('Gathered main branch of every remaining project')
 
@@ -57,14 +59,14 @@ with open ('list_branches.txt','r') as fin:
         data = line.split(';')
         if data[2].rstrip() != 'master':
             nonmaster = nonmaster + 1
-            os.system('rm -r -f /home/ubuntu/projects/' + data[0] + '/' + data[1])
+            os.system('rm -r -f ' + path + data[0] + '/' + data[1])
             print('// Deleted folder for', data[0], '/', data[1])
-            os.system('rm -f /home/ubuntu/projects/' + data[0] + '/' + data[1] + '.zip')
+            os.system('rm -f ' + path + data[0] + '/' + data[1] + '.zip')
             print('// Deleted zip for', data[0], '/', data[1])
-            os.system('git clone https://user:password@github.com/' + data[0] + '/' + data[1] + ' /home/ubuntu/projects/' + data[0] + '/' + data[1])
+            os.system('git clone https://user:password@github.com/' + data[0] + '/' + data[1] + ' ' + path + data[0] + '/' + data[1])
             print('// Created folder for', data[0], '/', data[1])
-            branch = cmdline('(cd /home/ubuntu/projects/' + data[0] + '/' + data[1] + '; git rev-parse --abbrev-ref HEAD)').decode('utf8').rstrip()
-            os.system('curl -L -o /home/ubuntu/projects/' + data[0] + '/' + data[1] + '.zip https://user:password@github.com/' + data[0] + '/' + data[1] + '/zipball/' + branch + '/')
+            branch = cmdline('(cd ' + path + data[0] + '/' + data[1] + '; git rev-parse --abbrev-ref HEAD)').decode('utf8').rstrip()
+            os.system('curl -L -o ' + path + data[0] + '/' + data[1] + '.zip https://user:password@github.com/' + data[0] + '/' + data[1] + '/zipball/' + branch + '/')
             print('// Created zip for', data[0], '/', data[1])
             print('Done for project', nonmaster)
             print('_____________________________________')
