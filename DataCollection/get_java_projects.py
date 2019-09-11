0b4534a5517eb0df89dc15b94aaa2a84a55f3a47
areyde <49271013+areyde@users.noreply.github.com>
2019-09-11 17:03:00 +0300

    Minor refactoring

--- DataCollection/get_java_projects.py
+++ DataCollection/get_java_projects.py
@@ -1,21 +1,20 @@
 import csv
+
 csv.field_size_limit(100000000)
-i = 0
-lst = []
+count = 0
 with open('java_projects_data.txt','w') as fout:
     with open('pga.csv', newline='') as fin:
         reader = csv.reader(fin)
         for row in reader:
             langs = row[3].split(',')
             if 'Java' in langs:
-                i = i + 1
+                count = count + 1
                 url = row[0].split('/')
                 author = url[-2]
                 name = url[-1]
                 if ':' not in row[13]:
                     row[13] = 'NONE'
                 lic = row[13]
-                lst.append([author,name,lic])
                 fout.write(author + ';' + name + ';' + lic + '\n')
                 
-print('Created pga.txt for',i,'Java projects')
+print('Created java_projects_data.txt for',count,'Java projects')
